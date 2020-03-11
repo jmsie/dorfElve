b340bd7d0ed705ee6f782714abeaf08c8c8ba675
jeff lu <jeff.lu0419@gmail.com>
2020-03-11 01:46:34 -0800

    Login function

--- capitalAPI/capitalAPI.py
+++ capitalAPI/capitalAPI.py
@@ -13,3 +13,16 @@ class CapitalAPI:
     self.skR = comtypes.client.CreateObject(sk.SKReplyLib, interface=sk.ISKReplyLib)
 
 
+  def login(self, id, password):
+    try:
+      self.skC.SKCenterLib_SetLogPath(os.path.split(os.path.realpath(__file__))[0] + "\\CapitalLog_Reply")
+      m_nCode = self.skC.SKCenterLib_Login(id, password)
+      self.skC.SKCenterLib_Debug(os.path.split(os.path.realpath(__file__))[0] + "\\CapitalLog_Reply")
+      if (m_nCode == 0):
+        print("Login success: " + id)
+      elif m_nCode == 151:
+        print("Wrong password")
+      else:
+        print("Login fail: " + m_nCode)
+    except Exception as e:
+      print("Error: " + e)
