ae4d39e88170776f588971e97499ab0b9ec2a45c
jeff lu <jeff.lu0419@gmail.com>
2020-03-11 23:57:47 -0800

    Move the SK objects to global

--- capitalAPI/capitalAPI.py
+++ capitalAPI/capitalAPI.py
@@ -5,29 +5,30 @@ from SKOrderLibEvent import SKOrderLibEvent
 from SKCenterLibEvent import SKCenterLibEvent
 from datetime import datetime
 
+comtypes.client.GetModule(os.path.split(os.path.realpath(__file__))[0] + r"/x64/SKCOM.dll")
+import comtypes.gen.SKCOMLib as sk
+
+skC = comtypes.client.CreateObject(sk.SKCenterLib, interface=sk.ISKCenterLib)
+skOOQ = comtypes.client.CreateObject(sk.SKOOQuoteLib, interface=sk.ISKOOQuoteLib)
+skO = comtypes.client.CreateObject(sk.SKOrderLib, interface=sk.ISKOrderLib)
+skOSQ = comtypes.client.CreateObject(sk.SKOSQuoteLib, interface=sk.ISKOSQuoteLib)
+skQ = comtypes.client.CreateObject(sk.SKQuoteLib, interface=sk.ISKQuoteLib)
+skR = comtypes.client.CreateObject(sk.SKReplyLib, interface=sk.ISKReplyLib)
+
+# comtypes使用此方式註冊callback
+SKReplyEvent = SKReplyLibEvent()
+SKReplyLibEventHandler = comtypes.client.GetEvents(skR, SKReplyEvent)
+
+# comtypes使用此方式註冊callback
+SKOrderEvent = SKOrderLibEvent()
+SKOrderLibEventHandler = comtypes.client.GetEvents(skO, SKOrderEvent)
+
+# comtypes使用此方式註冊callback
+SKCenterEvent = SKCenterLibEvent()
+SKCenterLibEventHandler = comtypes.client.GetEvents(skC, SKCenterEvent)
+
 class CapitalAPI:
   def __init__(self):
-    comtypes.client.GetModule(os.path.split(os.path.realpath(__file__))[0] + r"/x64/SKCOM.dll")
-    import comtypes.gen.SKCOMLib as sk
-    self.skC = comtypes.client.CreateObject(sk.SKCenterLib, interface=sk.ISKCenterLib)
-    self.skOOQ = comtypes.client.CreateObject(sk.SKOOQuoteLib, interface=sk.ISKOOQuoteLib)
-    self.skO = comtypes.client.CreateObject(sk.SKOrderLib, interface=sk.ISKOrderLib)
-    self.skOSQ = comtypes.client.CreateObject(sk.SKOSQuoteLib, interface=sk.ISKOSQuoteLib)
-    self.skQ = comtypes.client.CreateObject(sk.SKQuoteLib, interface=sk.ISKQuoteLib)
-    self.skR = comtypes.client.CreateObject(sk.SKReplyLib, interface=sk.ISKReplyLib)
-
-    # comtypes使用此方式註冊callback
-    SKReplyEvent = SKReplyLibEvent()
-    SKReplyLibEventHandler = comtypes.client.GetEvents(self.skR, SKReplyEvent)
-
-    # comtypes使用此方式註冊callback
-    SKOrderEvent = SKOrderLibEvent()
-    SKOrderLibEventHandler = comtypes.client.GetEvents(self.skO, SKOrderEvent)
-
-    # comtypes使用此方式註冊callback
-    SKCenterEvent = SKCenterLibEvent()
-    SKCenterLibEventHandler = comtypes.client.GetEvents(self.skC, SKCenterEvent)
-
     self.config = {
       "id": "",
       "account": "",
@@ -39,9 +40,9 @@ class CapitalAPI:
     self.config['account'] = account
 
   def login(self, id, password):
-    self.skC.SKCenterLib_SetLogPath(os.path.split(os.path.realpath(__file__))[0] + "\\CapitalLog_Reply")
-    m_nCode = self.skC.SKCenterLib_Login(id, password)
-    self.skC.SKCenterLib_Debug(os.path.split(os.path.realpath(__file__))[0] + "\\CapitalLog_Reply")
+    skC.SKCenterLib_SetLogPath(os.path.split(os.path.realpath(__file__))[0] + "\\CapitalLog_Reply")
+    m_nCode = skC.SKCenterLib_Login(id, password)
+    skC.SKCenterLib_Debug(os.path.split(os.path.realpath(__file__))[0] + "\\CapitalLog_Reply")
     if (m_nCode == 0):
       print("Login success: " + id)
       self.config['id'] = id
@@ -56,20 +57,14 @@ class CapitalAPI:
     self.set_order_limit()
 
   def initialize_SKOrderLib(self):
-    try:
-      print("Initializing SKOrderLib")
-      m_nCode = self.skO.SKOrderLib_Initialize()
-      self.write_message("Order", m_nCode, "SKOrderLib_Initialize")
-    except Exception as e:
-      print("error！" + e)
+    print("Initializing SKOrderLib")
+    m_nCode = skO.SKOrderLib_Initialize()
+    self.write_message("Order", m_nCode, "SKOrderLib_Initialize")
 
   def read_cert(self):
-    try:
-      print("Read cert...")
-      m_nCode = self.skO.ReadCertByID(self.config['id'])
-      self.write_message("Order", m_nCode, "ReadCertByID")
-    except Exception as e:
-      print("error！" + e)
+    print("Read cert...")
+    m_nCode = skO.ReadCertByID(self.config['id'])
+    self.write_message("Order", m_nCode, "ReadCertByID")
 
   def set_order_limit(self, limit=10):
     try:
@@ -77,21 +72,21 @@ class CapitalAPI:
       # 1 == Future
       self.config['quantity_limit'] = limit
       nMarketType = 1
-      m_nCode = self.skO.SetMaxQty(nMarketType, self.config['quantity_limit'])
+      m_nCode = skO.SetMaxQty(nMarketType, self.config['quantity_limit'])
       self.write_message("Order", m_nCode, "SetMaxQty")
-      m_nCode = self.skO.SetMaxCount(nMarketType, self.config['quantity_limit'])
+      m_nCode = skO.SetMaxCount(nMarketType, self.config['quantity_limit'])
       self.write_message("Order", m_nCode, "SetMaxCount")
     except Exception as e:
-      print("error！" + e)
+      print("error！" + str(e))
 
   def unlock_order_limit(self):
     try:
       # 1 == Future
       nMarketType = 1
-      m_nCode = self.skO.UnlockOrder(nMarketType)
+      m_nCode = skO.UnlockOrder(nMarketType)
       self.write_message("Order", m_nCode, "UnlockOrder")
     except Exception as e:
-      print ("error！" + e)
+      print ("error！" + str(e))
 
   '''
     sBuySell: sell(0), buy(1)
@@ -100,7 +95,7 @@ class CapitalAPI:
   '''
   def send_future_order(self, sBuySell, sTradeType, sDayTrade, quantity, bstrPrice="M", bAsyncOrder=False):
     try:
-      oOrder = self.sk.FUTUREORDER()
+      oOrder = sk.FUTUREORDER()
       oOrder.bstrFullAccount = self.config['account']
       oOrder.bstrStockNo = self.config['symbol']
       oOrder.sBuySell = sBuySell
@@ -111,10 +106,10 @@ class CapitalAPI:
       oOrder.sNewClose = 2  # AUTO
       oOrder.sReserved = 0  # T&&T+1
 
-      message, m_nCode = self.skO.SendFutureOrder(self.config['id'], bAsyncOrder, oOrder)
-      self.write_message("Order", m_nCode, "SendFutureOrder", self.__dOrder[''])
+      message, m_nCode = skO.SendFutureOrder(self.config['id'], bAsyncOrder, oOrder)
+      self.write_message("Order", m_nCode, "SendFutureOrder", "")
     except Exception as e:
-      print("error！" + e)
+      print("error！" + str(e))
 
   def buy_at_market(self, quantity=1):
     print("{}\tBuy {} at market".format(datetime.now(), quantity))
@@ -126,17 +121,17 @@ class CapitalAPI:
 
   def get_open_interest(self):
     try:
-      m_nCode = self.skO.GetOpenInterest(self.config['id'], self.config['account'])
+      m_nCode = skO.GetOpenInterest(self.config['id'], self.config['account'])
       self.write_message("Order", m_nCode, "GetOpenInterest")
     except Exception as e:
-      print("error！" + e)
+      print("error！" + str(e))
 
   def write_message(self, str_type, nCode, str_message):
     str_info = ""
     if (nCode != 0):
-      str_info = "【" + self.skC.SKCenterLib_GetLastLogInfo() + "】"
+      str_info = "【" + skC.SKCenterLib_GetLastLogInfo() + "】"
     print("【" + str_type + "】【" + str_message + "】【"
-          + self.skC.SKCenterLib_GetReturnCodeMessage(nCode) + "】" + str_info)
+          + skC.SKCenterLib_GetReturnCodeMessage(nCode) + "】" + str_info)
 
 if __name__ == "__main__":
   import  time;
@@ -149,6 +144,7 @@ if __name__ == "__main__":
   api.login(id, password)
   api.set_account(account)
   api.initialize_SKOrderLib()
+  exit()
   api.buy_at_market()
   api.sell_at_market(2)
   for i in range(30):
